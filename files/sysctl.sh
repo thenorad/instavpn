3df574ca33158cb1d62e5234963de939908b5966
Rostislav Vorobyev <rostik.vorobev@ya.ru>
2018-09-27 00:33:28 +0800

    edits

--- files/sysctl.sh
+++ files/sysctl.sh
@@ -6,5 +6,4 @@ echo "net.ipv4.conf.default.rp_filter = 0" |  tee -a /etc/sysctl.conf
 echo "net.ipv4.conf.default.accept_source_route = 0" |  tee -a /etc/sysctl.conf
 echo "net.ipv4.conf.default.send_redirects = 0" |  tee -a /etc/sysctl.conf
 echo "net.ipv4.icmp_ignore_bogus_error_responses = 1" |  tee -a /etc/sysctl.conf
-sysctl -p
-service procps restart
+sysctl -p /etc/sysctl.conf
